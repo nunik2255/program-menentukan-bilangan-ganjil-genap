cb006f4d8dc90ffb56a60c5528929a44231e06ab
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 16:44:47 +0700

    Add files via upload

---
+++ lat2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int bilangan;
+cout<<"masukan Bilangan"<<endl;
+cin>>bilangan;
+if(bilangan%2==0)
+cout<<"Bilangan Genap"<<endl;
+else
+cout<<"Bilangan ganjil"<<endl;
+return 0;
+}
